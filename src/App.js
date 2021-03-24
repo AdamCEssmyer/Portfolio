62afcb5f0dc83c8f67748914bd5544584e75854b
Adam Essmyer <acemyer@gmail.com>
2021-03-24 11:12:11 -0500

    started react portfolio site

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="me.jpg" className="" alt="profilepic" />
+        <p>
+          Adam Essmyer-Developer
+        </p>
+        <a
+          className="App-link"
+          href="C:\Users\theco\Coding Projects\package\package\exercise-1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View projects
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
